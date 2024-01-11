69cecd4675b649d1903781243c97a6e8a5193f16
Tercio Santos <terciofonseca@icloud.com>
2024-01-10 22:29:50 -0300

    ts-001 Starting the project Adding dependencies and configurations

---
+++ src/test/java/com/bhub/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhub.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
